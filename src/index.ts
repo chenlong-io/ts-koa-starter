08a65f911986d6e9743bca5629d3add5831c4500
chenlong <cl903254852@gmail.com>
2025-03-03 14:49:58 +0800

    feat：modify

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,16 @@ import * as Router from '@koa/router';
 import * as bodyParser from 'koa-bodyparser';
 import { PORT } from './config';
 import routesAction from './routes';
-import { responseMiddleware } from "./middleWare/responseMiddleWare";
+import { responseMiddleware } from './middleWare/responseMiddleWare';
 
 const app = new Koa();
 const router = new Router();
 
-// 应用中间件
-app.use(responseMiddleware)
-//路由
-AppRoutes.forEach(route => router[route.method](route.path, route.action));
-
 //路由及处理
 routesAction.forEach(({ path, type, action }) => router[type](path, action));
 
+// 应用中间件
+app.use(responseMiddleware);
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
